ed2cd7e56e3e1bc2af5f47380d8faab5196339d8
RosiAndrade <rosilaineaandrade@yahoo.com>
2024-10-11 20:22:36 -0300

    inicio

---
+++ Atividade 01.java
@@ -0,0 +1,11 @@
+//Rosilaine Souza Andrade
+//Desenvolvimento de Aplicações
+//TCTG241CNTDEV
+
+Public class Atividade 01 {
+
+Public static void main (String args[]){
+Int ID = 123456789
+String nome = Amanda
+Int cpf = 02315645612
+int Data = 16/05/1985
