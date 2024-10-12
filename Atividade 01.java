3745ccc531648e7830b605b8492ee40a8ed7b3ff
Naiara Carmo <naiaracbmaciel@gmail.com>
2024-10-11 21:13:52 -0300

    adicionando link no ReadMe

--- Atividade 01.java
+++ Atividade 01.java
@@ -2,6 +2,10 @@
 //Desenvolvimento de Aplicações
 //TCTG241CNTDEV
 
+Link do drive onde está os exercícios que devemos entregar até o fim do curso:
+<br>
+<a href="https://drive.google.com/drive/folders/1QPRn3Nu7jB8G8k7AnwktrLJC4oQGh0Ls"> Drive com exercícios </a>
+
 Public class Atividade 01 {
 
 Public static void main (String args[]){
